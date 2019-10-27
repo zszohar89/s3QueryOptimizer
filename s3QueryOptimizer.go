bb77465d65fd3750016ef9abecfdcb51196904ef
zohar <zohar@appsflyer.com>
2019-10-27 12:28:05 +0200

    wip

--- s3QueryOptimizer.go
+++ s3QueryOptimizer.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"strings"
 )
 
+type SparkQueryPlan struct {
+	Event string
+	ExecutionId int32
+	Description string
+	Details string
+	PhysicalPlanDescription string
+	SparkPlanInfo string
+	Time string
+}
 
 func readLines(path string) ([]string, error) {
 	data, err := ioutil.ReadFile(path)
-	if data != nil {
+	if err != nil {
+		log.Print("got an error when trying to load data from file")
 		return nil, err
 	} else {
-		log.Print(strings.Split(string(data), "\n"))
 		return strings.Split(string(data), "\n"), nil
 	}
 
@@ -23,27 +33,39 @@ func extractSQLExecutionPlanEvents(data []string) []string {
 	for _, event := range data {
 		if strings.Contains(event, "org.apache.spark.sql.execution.ui.SparkListenerSQLExecutionStart") {
 			sqlPlans = append(sqlPlans, event)
-			log.Print(sqlPlans)
 		}
 	}
 	return sqlPlans
 }
 
-func parseSqlPlan(sqlPlan string) {
+func unmarshalSqlExecutionPlanEvent(sqlPlan string) (SparkQueryPlan, error) {
+	var sparkQueryPlan SparkQueryPlan
+	err := json.Unmarshal([]byte(sqlPlan), &sparkQueryPlan)
+	if err == nil {
+		return sparkQueryPlan, nil
+	} else {
+		return sparkQueryPlan, err
+	}
+}
 
+func parsePhysicalPlan(PhysicalPlanDescription string) {
+	strings.Split(string(data), "\n")
 }
 
 
 func main() {
 	splitData, err := readLines("application_1570695864101_4014_1")
-	log.Print(splitData)
 	if err != nil {
 		log.Fatal(err)
 	} else {
 	sqlPlans := extractSQLExecutionPlanEvents(splitData)
 		if len(sqlPlans) != 0 {
 			for _, sqlPlan := range sqlPlans {
-				parseSqlPlan(sqlPlan)
+				sparkQueryPlan, err := unmarshalSqlExecutionPlanEvent(sqlPlan)
+				if err == nil {
+					parsePhysicalPlan(sparkQueryPlan.PhysicalPlanDescription)
+				}
+
 			}
 
 		} else {
--- s3QueryOptimizer_test.go
+++ s3QueryOptimizer_test.go
@@ -1,2 +1,30 @@
 package main
 
+import "testing"
+
+func TestParseSqlPlan(t *testing.T) {
+	var sqlPlanEvent = `
+{"Event" : "org.apache.spark.sql.execution.ui.SparkListenerSQLExecutionStart",
+"executionId" : 0,
+"description" : "parquet at PerformanceTest.scala:169",
+"details" : "test",
+"physicalPlanDescription" : "physicalPlanDescription-test",
+"sparkPlanInfo" : "sparkPlanInfo-test",
+"time" : "1234"}
+`
+
+ expected := SparkQueryPlan{"org.apache.spark.sql.execution.ui.SparkListenerSQLExecutionStart",0,
+ 	"parquet at PerformanceTest.scala:169", "test", "physicalPlanDescription-test", "sparkPlanInfo-test",
+ "1234"}
+
+ parseSqlPlanOutPut, err := unmarshalSqlExecutionPlanEvent(sqlPlanEvent)
+ if err == nil {
+ 	if expected != parseSqlPlanOutPut{
+ 	t.Errorf("function returned  wrong error : got %v want %v", parseSqlPlanOutPut, expected)
+ 	}
+ } else {
+	 t.Errorf("something bad happend error is %s", err.Error())
+ }
+
+
+}
